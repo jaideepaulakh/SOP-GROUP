5ba0e6d751ce63735cc15bbcc6dea93b06a8eb18
jaideepaulakh <133963566+jaideepaulakh@users.noreply.github.com>
2024-04-28 14:47:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,23 +2,21 @@
 window.addEventListener('scroll', function() {
     // Get the header element
     const header = document.querySelector('.header-div');
+    const navdiv = document.querySelector('.nav-desktop');
     // Add 'scrolled' class to header when scrolling down, remove it when scrolling back to the top
     if (window.scrollY > 0) {
         header.classList.add('scrolled');
+        navdiv.classList.add("nav-desktop2");
+
     } else {
         header.classList.remove('scrolled');
+        navdiv.classList.remove('nav-desktop2');
     }
 });
 
-
-// JavaScript to scroll to the about section when clicked
-document.addEventListener('DOMContentLoaded', function () {
-    const aboutLink = document.querySelector('.home');
-    const aboutSection = document.getElementsById('about-section');
-
-    aboutLink.addEventListener('click', function(event) {
-
-        // Scroll to the about section smoothly
-        aboutSection.scrollIntoView({ behavior: 'smooth' });
-    });
-});
+const menuiconContainer = document.querySelector(".menu-icon-container");
+const navContainer = document.querySelector(".nav-desktop");
+menuiconContainer.addEventListener("click", () => {
+    menuiconContainer.classList.toggle("active");
+    navContainer.classList.toggle("active-nav")
+})
